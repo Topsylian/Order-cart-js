dd7fd36a4e93a919a7f304c1e4728ab240a1959b
Topsylian <topsylian@gmail.com>
2023-02-21 14:39:42 +0200

    ggg

---
+++ script.js
@@ -0,0 +1,41 @@
+const btn = document.querySelector("button");
+const select = document.querySelector("select");
+const h1 = document.querySelector("h1");
+const html = document.querySelector("html");
+const list = document.querySelector("ul");
+
+let days = 31;
+h1.innerHTML = 'January';
+
+select.addEventListener("change", () => {
+	const choice = select.value;
+	
+  if (choice === "february") {
+    days = 28;
+  } else if (
+    choice === "september" ||
+    choice === "april" ||
+    choice === "june" ||
+    choice === "november"
+  ) {
+    days = 30;
+  } else {
+    days = 31;
+	}
+
+	function createCalender(days, choice) {
+		list.innerHTML = "";
+		h1.textContent = choice;
+	
+		for (let i = 1; i <= days ; i++) {
+			const listDate = document.createElement("li");
+			listDate.textContent = i;
+			console.log(i);
+			list.appendChild(listDate);
+		}
+	}
+	
+	createCalender(days, choice);
+});
+
+
